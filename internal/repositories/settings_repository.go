b1dc4f540cbcc536bf1427988d3d5b21ba846c28
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-06-17 19:19:13 +0500

    settings

--- internal/repositories/settings_repository.go
+++ internal/repositories/settings_repository.go
@@ -19,7 +19,7 @@ func (r *SettingsRepository) Get(ctx context.Context) (*models.Settings, error) 
 	query := `
 		SELECT s.id, s.payment_type, s.block_time, s.bonus_percent, s.work_time_from, s.work_time_to
 		FROM settings s 	
-		JOIN payment_types pt ON s.payment_type = pt.id
+		LEFT JOIN payment_types pt ON s.payment_type = pt.id
 		LIMIT 1
 	`
 	var s models.Settings
