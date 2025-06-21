e4775c9b7cd14345e588fd745e93bcc85c124ace
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-06-21 19:44:23 +0500

    category id null

--- internal/models/expense.go
+++ internal/models/expense.go
@@ -1,18 +1,15 @@
 package models
 
-import (
-	"database/sql"
-	"time"
-)
+import "time"
 
 type Expense struct {
-	ID          int           `json:"id"`
-	Date        time.Time     `json:"date"`
-	Title       string        `json:"title"`
-	CategoryID  sql.NullInt64 `json:"category_id"`
-	Category    string        `json:"category"`
-	Total       float64       `json:"total"`
-	Description string        `json:"description"`
-	Paid        bool          `json:"paid"`
-	CreatedAt   time.Time     `json:"created_at"`
+	ID          int       `json:"id"`
+	Date        time.Time `json:"date"`
+	Title       string    `json:"title"`
+	CategoryID  int       `json:"category_id,omitempty"`
+	Category    string    `json:"category"`
+	Total       float64   `json:"total"`
+	Description string    `json:"description"`
+	Paid        bool      `json:"paid"`
+	CreatedAt   time.Time `json:"created_at"`
 }
