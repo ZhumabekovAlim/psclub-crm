0d74fa83f9f6afbc490d370d090c09f75d6dcf9e
Alim Zhumabekov <145676784+ZhumabekovAlim@users.noreply.github.com>
2025-09-15 12:45:44 +0500

    Include category name in price list and set responses

--- internal/models/price_set.go
+++ internal/models/price_set.go
@@ -8,6 +8,7 @@ type PriceSet struct {
 	Quantity        float64   `json:"quantity"`
 	Price           int       `json:"price"`
 	SubcategoryName string    `json:"subcategory_name,omitempty"`
+	CategoryName    string    `json:"category_name,omitempty"`
 	CompanyID       int       `json:"company_id"`
 	BranchID        int       `json:"branch_id"`
 	Items           []SetItem `json:"items"`
